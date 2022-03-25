34f043b533716110c8e0f556c1f6bc5bb629252e
WizardlyDeveloper <wizardlydeveloper@gmail.com>
2022-03-25 13:55:56 +0530

    added moralis

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,10 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { MoralisProvider } from "react-moralis";
+import 'dotenv/config';
+
+const { appId, serverUrl } = process.env
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MoralisProvider appId={appId} serverUrl={serverUrl}>
+      <App />
+    </MoralisProvider>,
   </React.StrictMode>,
   document.getElementById('root')
 );
